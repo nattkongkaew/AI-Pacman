b64088642eb25bb28d56ac14bef35f1fbd4914bd
Violet Kurtz <kody.j.kurtz@gmail.com>
2020-05-05 20:18:18 -0700

    Refactor: Created Board scene with full one-player game.

--- Pellet.gd
+++ Pellet.gd
@@ -1,18 +1,12 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 
 
 func _on_Pellet_area_entered(_area):
 	queue_free()
-	pass # Replace with function body.

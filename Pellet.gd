6575ccaa58d42d925954c38db9315b5529b456d4
kongkaen <56180077+kongkaen@users.noreply.github.com>
2020-05-08 12:50:43 -0700

    Add files via upload

--- Pellet.gd
+++ Pellet.gd
@@ -1,10 +1,21 @@
 extends Area2D
 
 
+signal pellet_collected( score )
+
+const COLLECTION_SCORE = 10
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 
+
+
+
+
+
 func _on_Pellet_body_entered(body: Node) -> void:
+	emit_signal( "pellet_collected", COLLECTION_SCORE )
 	queue_free()

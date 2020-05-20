2a8b938b5f55584e119da3594b75ce3b86cb6a00
kongkaen <56180077+kongkaen@users.noreply.github.com>
2020-05-20 10:23:35 -0700

    Add files via upload

--- Pinky.gd
+++ Pinky.gd
@@ -6,7 +6,7 @@ var direction = Vector2(0,0)
 var SPEED = 50
 
 func _ready():
-	position = walls.get_enemy_pos()
+	position = walls.get_pinky_pos()
 	path = walls.get_path_to_player()
 	
 func _physics_process(delta):
--- Walls.gd
+++ Walls.gd
@@ -1,13 +1,9 @@
 extends TileMap
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
 onready var half_cell_size = get_cell_size()/2
 onready var player = get_parent().get_parent().get_node("Pacman")
-onready var enemy = get_parent().get_parent().get_node("Pinky")
+onready var pinky = get_parent().get_parent().get_node("Pinky")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,11 +16,11 @@ func _ready() -> void:
 #	pass
 onready var walls = get_parent().get_node("Walls")
 
-func get_enemy_pos():
+func get_pinky_pos():
 	var pos = map_to_world(Vector2(16,19))
 	pos.y += half_cell_size.y
 	return pos
 
 func get_path_to_player():
-	var path = get_parent().get_simple_path(enemy.position, player.position, false)
+	var path = get_parent().get_simple_path(pinky.position, player.position, false)
 	return path

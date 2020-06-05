1420f16dbfd9bee77ba7630032b56bb5240f937e
Rebekah <koonr@oregonstate.edu>
2020-06-05 14:03:49 -0700

    Score increases when power pellet collected

--- PowerPellet.gd
+++ PowerPellet.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal power_pellet_collected
+onready var BoardScoreboard = get_parent().get_parent().get_parent().get_node("BoardScoreboard")
 
 func _ready():
 	pass
@@ -8,4 +9,5 @@ func _ready():
 
 func _on_PowerPellet_body_entered(_body: Node) -> void:
 	emit_signal("power_pellet_collected")
+	BoardScoreboard.add_score(40)
 	queue_free()

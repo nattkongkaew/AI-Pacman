44c01d1a12209d1eb62d0aef83137a182c21c173
Rebekah <koonr@oregonstate.edu>
2020-06-05 13:51:44 -0700

    Score increases when fruit collected

--- Pellet.gd
+++ Pellet.gd
@@ -4,6 +4,7 @@ extends Area2D
 signal pellet_collected( score )
 
 const COLLECTION_SCORE = 10
+onready var Walls = get_parent().get_parent()
 
 
 # Called when the node enters the scene tree for the first time
@@ -12,4 +13,6 @@ func _ready():
 
 func _on_Pellet_body_entered(_body: Node) -> void:
 	emit_signal( "pellet_collected", COLLECTION_SCORE )
+	Walls.add_pellet_count()
 	queue_free()
+
